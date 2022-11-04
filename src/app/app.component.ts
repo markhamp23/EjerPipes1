6030fff6efa2c0d84ba19482f9460443359fd4c4
markhamp23 <markhamp23@gmail.com>
2022-11-04 01:06:52 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EjerPipes1';
+  nombre="jorge fernandez PUGA";
+  pasta=12;
+  fechaHoy= new Date();
+  alumnos=['Jorge', 'Patrick', 'Lola', 'Jaime','Raul','David'];
+  meses=[1,2,3,4,5,6,7,8,9,10,11,12];
 }
