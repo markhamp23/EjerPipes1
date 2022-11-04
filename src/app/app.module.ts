b9e58abaa6c2f4711dcb4787de0dcf6a7dd06011
markhamp23 <markhamp23@gmail.com>
2022-11-04 01:16:50 +0100

    Error on app.module.ts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MesesTextoPipe } from './meses-texto.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MesesTextoPipe
   ],
   imports: [
     BrowserModule,
